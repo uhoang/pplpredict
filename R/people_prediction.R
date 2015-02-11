8cd3da6584a99570e5f8973b3094c2edec337037
Uyen <uyen@voxpoplabs.com>
2015-02-11 17:59:40 -0500

    Modified rescaleWrapper and peoplePrediciton functions; added aliases for functions in people_prediction.R file.

--- R/people_prediction.R
+++ R/people_prediction.R
@@ -1,20 +1,43 @@
-#' Rescale a number from 0-10 to 0-1 scale.
+#' Rescale vector elements.
 #'
+#' \code{rescaleWrapper} returns the vector of values rescale to 0-1 from 0-10 scale 
+#' @aliases resecaleWrapper rescale rescaled
 #' @param x A numeric number or vector.
 #' @return The rescaled number or vector of \code{x} 
 #' @examples 
-#' rescaleWrapper(c(1, 2, 3))
-#' rescaleWrapper(c(0, 1, NA))
-rescaleWrapper <- function(x){
-  x[is.na(x)] <- 0
-  x <- x/10
-  x[x > 1] <- 1
-  x[x < 0 ] <- 0
+#' rescaleWrapper(c(1, 2, 3), range = c(0,1))
+#' rescaleWrapper(c(0, 1, NA), range = c(0,1))
+#' rescaleWrapper(c(0, 1, 5, -3, NA), domain = c(0, 10), range = c(0,1), bounded = FALSE, na.replace = NA)
+#' rescaleWrapper(c(0, 1, 5, -3, NA), domain = c(0, 10), range = c(0,1), bounded = TRUE, na.replace = NA)
+rescaleWrapper <- function(x, domain = NULL, range = NULL, bounded = FALSE, na.replace = NA){
+  
+  if(is.null(domain)){
+    dmax <- max(x, na.rm = TRUE)
+    dmin <- min(x, na.rm = TRUE)
+  }else{
+    dmax <- domain[2]
+    dmin <- domain[1]
+  }
+
+  if(is.null(range)) {
+    stop("The mapping requires the range of x.")
+  }else{
+    rmax <- range[2]
+    rmin <- range[1]
+  }
+  
+  x <- rmin + (x - dmin)/(dmax - dmin)*(rmax - rmin)
+  
+  if(bounded){
+    x[x < rmin] <- rmin
+    x[x > rmax] <- rmax 
+  }
+  x[is.na(x)] <- na.replace
   x 
 }
 
 #' Find an index of a maximum value in a numeric vector.
-#'
+#' @aliases maxWrapper max maximum
 #' @param x A numeric vector 
 #' @return A index of a maximum value in \code{x}
 #' @examples
@@ -26,23 +49,9 @@ maxWrapper <- function(x){
   idx 
 }
 
-# #' Calculate a relative confidence index between a winner and other candidates.
-# #'
-# #' @param x A matrix or data frame with n first columns are the winning ratings for n parties and the last column is the column index of a winner.
-# #' @param nLeaders A number of parties in the election race.
-# #' @return A matrix or data frame with n columns of relative confidence index for each party.
-# #' @examples 
-# #' RCIMeasure(cbind(matrix(1:9, 3,3), c(1,1,3)), 3)
-# #' RCIMeasure(data.frame(matrix(1:18, 6,3), c(1,1,2,2,2,1)), 3)
-# RCIMeasure <- function(x, nLeaders) {
-#   winnerIdx <- nLeaders + 1
-#   rci <- x[1:nLeaders] - x[x[winnerIdx]]
-#   rci[x[winnerIdx]] <- x[x[winnerIdx]] - sort(x[-x[winnerIdx]], decreasing = TRUE)[2]
-#   rci  
-# }
 
 #' Calcualte a relative confidence indexn between a winner and other candidates.
-#'
+#' @aliases RCIMeasure RCI rci 
 #' @param x A numerical vector of the winning ratings for each party.
 #' @param partyNames A character vector of party names in the election race.
 #' @examples 
@@ -61,7 +70,7 @@ RCIMeasure <- function(x, partyNames = NULL){
 
 
 #' Calulate a people Prediction at electorate levels using relative confidence index 
-#' 
+#' @aliases peoplePrediction pplpredict peoplepredict
 #' @param Data A data frame of VoteCompass datasets 
 #' @param rci.vars A character vector or a numeric vector of index of variable names of the winning ratings for each party.
 #' @param partyNames A character vector of full names for each party.
@@ -69,18 +78,18 @@ RCIMeasure <- function(x, partyNames = NULL){
 #' @examples 
 #' peoplePrediction(Queensland, paste0("party", 1:4, "B"), partyNames = c("Labor","Greens","LibNat","Katter"))
 #' peoplePrediction(Queensland, paste0("party", 1:3, "B"), partyNames = c("Labor","Greens","LibNat"))
-# Data <- Queensland
-# rci.vars <- paste0("party",1:4,"B")
-# partyNames = c("Labor","Greens","LibNat","Katter")
-# transform = TRUE
-
 peoplePrediction <- function(Data, rci.vars, partyNames = NULL, transform = TRUE, ...){
 
     if(!is.data.frame(Data)) stop("Data has a wrong format. It must be a data.frame.")
     Rate <- Data[, rci.vars]
-    
-    if(transform) Rate <- data.frame(apply(Rate, 2, rescaleWrapper))
-    
+    args <- match.call()
+    if(transform) { 
+      if(!"domain" %in% names(args)) domain = c(0,10)
+      if(!"range" %in% names(args)) range = c(0,1)
+      if(!"bounded" %in% names(args)) bounded = TRUE
+      if(!"na.repalce" %in% names(args)) na.replace = 0
+      Rate <- data.frame(apply(Rate, 2, rescaleWrapper, domain = domain, range = range, bounded = bounded, na.replace = bounded))
+    }
     RCI <- apply(Rate, 1, function(x) RCIMeasure(x, partyNames))
     RCI <- data.frame(do.call(rbind, lapply(RCI,unlist)), stringsAsFactors = FALSE)
     
